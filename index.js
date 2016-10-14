c26c595b093b0f18398166e79abf28cc3d0351e9
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2016-10-14 10:46:59 +0900

    remove Promise polyfill
    no longer supports Node v0.10.0

--- index.js
+++ index.js
@@ -7,31 +7,30 @@
 var inspect = require('util').inspect;
 
 var fs = require('graceful-fs');
-var PinkiePromise = require('pinkie-promise');
 
 var PATH_ERROR = 'Expected a file path (string) to open and resolve its file descriptor';
 var FLAG_ERROR = 'Expected valid file open flag, for example \'r\' & \'ax+\'';
 
 module.exports = function openFile(filePath, flags, mode) {
   if (typeof filePath !== 'string') {
-    return PinkiePromise.reject(new TypeError(PATH_ERROR + ', but got ' + inspect(filePath) + '.'));
+    return Promise.reject(new TypeError(PATH_ERROR + ', but got ' + inspect(filePath) + '.'));
   }
 
   if (filePath.length === 0) {
-    return PinkiePromise.reject(new Error(PATH_ERROR + ', but got an empty string.'));
+    return Promise.reject(new Error(PATH_ERROR + ', but got an empty string.'));
   }
 
   var typeOfFlags = typeof flags;
 
   if (typeOfFlags !== 'string' && typeOfFlags !== 'number') {
-    return PinkiePromise.reject(new TypeError(FLAG_ERROR + ', but got ' + inspect(flags) + '.'));
+    return Promise.reject(new TypeError(FLAG_ERROR + ', but got ' + inspect(flags) + '.'));
   }
 
   if (flags === '') {
-    return PinkiePromise.reject(new Error(FLAG_ERROR + ', but got an empty string.'));
+    return Promise.reject(new Error(FLAG_ERROR + ', but got an empty string.'));
   }
 
-  return new PinkiePromise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.open(filePath, flags, mode, (err, fileDescriptor) => {
       if (err) {
         reject(err);
