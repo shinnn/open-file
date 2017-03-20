49e0e249a335dd9801a67097a7e40cdfd72caa72
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-03-20 19:46:59 +0900

    simplify index.js
    * use destructuring assignment
    * use `throw` instead of `return Promise.reject(…)`
    
    NOTE: This introduces a breaking change because destructuring assignment requires Node.js >= 6

--- index.js
+++ index.js
@@ -4,34 +4,34 @@
 */
 'use strict';
 
-const inspect = require('util').inspect;
+const {inspect} = require('util');
 
-const fs = require('graceful-fs');
+const fsOpen = require('graceful-fs').open;
 
 const PATH_ERROR = 'Expected a file path (string) to open and resolve its file descriptor';
 const FLAG_ERROR = 'Expected valid file open flag, for example \'r\' & \'ax+\'';
 
 module.exports = function openFile(filePath, flags, mode) {
-  if (typeof filePath !== 'string') {
-    return Promise.reject(new TypeError(`${PATH_ERROR}, but got ${inspect(filePath)}.`));
-  }
-
-  if (filePath.length === 0) {
-    return Promise.reject(new Error(`${PATH_ERROR.replace(' (string)', '')}, but got '' (empty string).`));
-  }
-
-  const typeOfFlags = typeof flags;
-
-  if (typeOfFlags !== 'string' && typeOfFlags !== 'number') {
-    return Promise.reject(new TypeError(`${FLAG_ERROR}, but got ${inspect(flags)}.`));
-  }
-
-  if (flags === '') {
-    return Promise.reject(new Error(`${FLAG_ERROR}, but got '' (empty string).`));
-  }
-
   return new Promise((resolve, reject) => {
-    fs.open(filePath, flags, mode, (err, fileDescriptor) => {
+    if (typeof filePath !== 'string') {
+      throw new TypeError(`${PATH_ERROR}, but got ${inspect(filePath)}.`);
+    }
+
+    if (filePath.length === 0) {
+      throw new Error(`${PATH_ERROR.replace(' (string)', '')}, but got '' (empty string).`);
+    }
+
+    const typeOfFlags = typeof flags;
+
+    if (typeOfFlags !== 'string' && typeOfFlags !== 'number') {
+      throw new TypeError(`${FLAG_ERROR}, but got ${inspect(flags)}.`);
+    }
+
+    if (flags === '') {
+      throw new Error(`${FLAG_ERROR}, but got '' (empty string).`);
+    }
+
+    fsOpen(filePath, flags, mode, (err, fileDescriptor) => {
       if (err) {
         reject(err);
         return;
